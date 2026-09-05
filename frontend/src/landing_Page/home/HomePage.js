731d0600404ebe4872485ec0a6b6288bba66f410
Aashish Mulaniya <mulaniyaaashish@gmail.com>
2026-09-05 21:44:43 +0530

    making home page

---
+++ frontend/src/landing_Page/home/HomePage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+function HomePage() {
+    return ( 
+        <>
+        <NavBar />
+        <Hero />
+        <OpenAccount />
+        <Trust />
+        <Connect />
+        <Pricing />
+        <Education />
+        <OpenAccount />
+        <Footer />
+        </>
+     );
+}
+
+export default HomePage;
