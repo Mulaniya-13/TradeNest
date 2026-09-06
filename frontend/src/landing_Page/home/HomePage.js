304d117ffdd09e9f466149c2b0b8178b18893565
Aashish Mulaniya <mulaniyaaashish@gmail.com>
2026-09-06 16:22:51 +0530

    home page making

--- frontend/src/landing_Page/home/HomePage.js
+++ frontend/src/landing_Page/home/HomePage.js
@@ -1,4 +1,12 @@
 import React from 'react';
+import Connect from './Connect';
+import Pricing from './Pricing';
+import NavBar from '../NavBar';
+import Hero from './Hero';
+import OpenAccount from '../OpenAccount';
+import Trust from './Trust';
+import Education from './Education';
+import Footer from '../Footer';
 
 
 function HomePage() {
