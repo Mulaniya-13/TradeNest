59fbc4a3a22a6e7ee5ecb37124117c3ed0106622
Aashish Mulaniya <mulaniyaaashish@gmail.com>
2026-09-16 21:30:54 +0530

    products

--- frontend/src/landing_Page/products/ProductsPage.js
+++ frontend/src/landing_Page/products/ProductsPage.js
@@ -49,8 +49,8 @@ function ProductsPage() {
         googlePlay=""
         appStore=""
       />
+      <p className="mt-5 mb-5 text-center fs-5">Want to know more about our technology stack? Check out the <a href="">Zerodha.tech</a> blog.</p>
       <Universe />
-      <OpenAccount />
     </>
   );
 }
